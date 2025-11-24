b5a0caa9eec567b547b09f08a6c9b595b5e394be
Abhirath1906 <abhirath1906@gmail.com>
2025-11-24 10:14:18 +0700

    first commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
-import { Layout, Divider, Table, Skeleton, message } from "antd"
+import { Layout, Divider, Table, Skeleton, message, Button } from "antd"
+
 
 const { Header, Content } = Layout
 
@@ -7,18 +8,23 @@ export default function Home() {
   const [TheApi, setTheApi] = useState([])
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    async function fetchApi() {
-      try {
-        const res = await fetch("https://jsonplaceholder.typicode.com/todos")
-        const data = await res.json()
-        setTheApi(data)
-      } catch {
-        message.error("Error bro")
-      } finally {
-        setLoading(false)
-      }
+
+
+  async function fetchApi() {
+    setLoading(true)
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const data = await res.json()
+      setTheApi(data)
+    } catch {
+      message.error("Error bro")
+    } finally {
+      setLoading(false)
     }
+  }
+
+
+  useEffect(() => {
 
     fetchApi()
   }, [])
@@ -26,6 +32,7 @@ export default function Home() {
   const cl = [
     { title: "ID", dataIndex: "id", key: "id" },
     { title: "Title", dataIndex: "title", key: "title" },
+
   ]
 
   return (
@@ -38,6 +45,8 @@ export default function Home() {
         <Content style={{ padding: "50px", backgroundColor: "white" }}>
           <p style={{ fontSize: "40px", fontWeight: "500" }}>The List</p>
           <Divider />
+
+          <Button style={{marginBottom:"10px"}} type="primary" onClick={fetchApi}>Refreshh.....</Button>
 
           {loading ? (
             <Skeleton active paragraph={{ rows: 15 }} />
